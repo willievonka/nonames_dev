fa7d6fbdfecd466c4d65c680545a9a99cc2a43b3
willie_vonka <nikolai.klimov@urfu.me>
2025-03-19 17:28:27 +0500

    Сделали футер и доработали tui-flat-button

--- nespitsya/src/app/app.component.ts
+++ nespitsya/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { TuiRoot } from '@taiga-ui/core';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, TuiRoot, HeaderComponent],
+    imports: [RouterOutlet, TuiRoot, HeaderComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
--- nespitsya/src/app/components/footer/footer.component.ts
+++ nespitsya/src/app/components/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiFlatButtonComponent } from '../tui-flat-button/tui-flat-button.component';
 
 @Component({
     selector: 'app-footer',
-    imports: [],
+    imports: [TuiFlatButtonComponent],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
-
+    public flatButtonIconLeft: string = 'Связаться с нами';
+    public flatButtonIconRight: string = '@tui.flag';
+    public flatButtonSize: 's' | 'm' | 'l' | 'xl' | 'xs' = 'xs';
 }
