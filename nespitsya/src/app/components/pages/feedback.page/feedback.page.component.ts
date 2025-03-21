0e9aeddd0a9ef895201c75ff286a1b83ac58f35a
EduardKostylev <edik03032005@yandex.ru>
2025-03-21 16:42:36 +0500

    Небольшие правки в странице формы

--- nespitsya/src/app/components/pages/feedback.page/feedback.page.component.ts
+++ nespitsya/src/app/components/pages/feedback.page/feedback.page.component.ts
@@ -1,17 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiBreadcrumbsComponent } from '../../tui-components/tui-breadcrumbs/tui-breadcrumbs.component';
 import { TuiInputComponent } from '../../tui-components/tui-input/tui-input.component';
+import { TuiAccentButtonComponent } from '../../tui-components/tui-accent-button/tui-accent-button.component';
 
 @Component({
     selector: 'app-feedback-page',
     imports: [
         TuiBreadcrumbsComponent,
         TuiInputComponent,
+        TuiAccentButtonComponent,
     ],
     templateUrl: './feedback.page.component.html',
     styleUrl: './feedback.page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeedbackPageComponent {
-
+  public sendButtonIcon: string = '@tui.mail';
+  public sendButtonText: string = 'Отправить';
 }
