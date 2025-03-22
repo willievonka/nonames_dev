baccf6063e87ff5a7baacd2ef831bae76216ed69
willie_vonka <nikolai.klimov@urfu.me>
2025-03-22 15:58:39 +0500

    Сделал ссылку с карточки на ивент, доработал стили

--- nespitsya/src/app/components/tui-components/tui-event-card/tui-event-card.component.ts
+++ nespitsya/src/app/components/tui-components/tui-event-card/tui-event-card.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
-import { TuiAppearance, TuiTitle } from '@taiga-ui/core';
+import { TuiAppearance } from '@taiga-ui/core';
 import { TuiCardLarge } from '@taiga-ui/layout';
 import { TuiChipComponent } from '../tui-chip/tui-chip.component';
-
+import { RouterLink } from '@angular/router';
 
 
 @Component({
@@ -11,6 +11,7 @@ import { TuiChipComponent } from '../tui-chip/tui-chip.component';
     imports: [
         CommonModule,
         DatePipe,
+        RouterLink,
         TuiCardLarge,
         TuiAppearance,
         TuiChipComponent,
@@ -20,10 +21,11 @@ import { TuiChipComponent } from '../tui-chip/tui-chip.component';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TuiEventCardComponent {
-    public eventImage: string = 'https://i.imgur.com/5qSSGHi.jpeg';
-    public eventTitle: string = 'Оркестр CAGMO | Концерт при свечах';
-    public eventPlace: string = 'Детская филармония';
-    public eventDate: Date = new Date ('2025-03-12T12:00:00');
-    public eventPrice: string = 'от 1500₽';
-    public eventTags: string[] = ['Классическая музыка', 'Саундтрек', 'Неоклассика', 'Шоу', 'Концерт'];
+    public image: string = 'https://i.imgur.com/5qSSGHi.jpeg';
+    public title: string = 'Оркестр CAGMO | Концерт при свечах';
+    public place: string = 'Детская филармония';
+    public date: Date = new Date ('2025-03-12T12:00:00');
+    public price: string = 'от 1500₽';
+    public tags: string[] = ['Классическая музыка', 'Саундтрек', 'Неоклассика', 'Шоу', 'Концерт'];
+    public routerLink: string = '';
 }
