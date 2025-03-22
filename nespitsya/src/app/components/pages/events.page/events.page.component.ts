2cfece191e326dc90acd9f0a63ad3a77e6d23197
willie_vonka <nikolai.klimov@urfu.me>
2025-03-22 21:21:41 +0500

    Создал компоненту страницы ивентов

---
+++ nespitsya/src/app/components/pages/events.page/events.page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-events.page',
+    imports: [],
+    templateUrl: './events.page.component.html',
+    styleUrl: './events.page.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EventsPageComponent {
+
+}
